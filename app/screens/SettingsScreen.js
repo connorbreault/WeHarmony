3ac7afed8feba73608606c5d74ebf6b630467b9e
Spaghetti Jones <connor.breault@comcast.net>
2021-03-03 17:32:25 -0800

    settings screen header added

--- app/screens/SettingsScreen.js
+++ app/screens/SettingsScreen.js
@@ -1,20 +1,54 @@
 import React from "react";
-import { View, ScrollView, StyleSheet } from "react-native";
+import {
+  View,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+} from "react-native";
+
+import colors from "../config/colors";
 
 import Screen from "../components/Screen";
 import AppText from "../components/Text";
 
-function SettingsScreen(props) {
+import Icon from "../components/Icon";
+
+function SettingsScreen({ navigation }) {
   return (
-    <Screen>
-      <ScrollView>
-        <AppText>Settings</AppText>
-      </ScrollView>
-    </Screen>
+    <>
+      <Screen style={styles.screen}>
+        <View style={styles.headerContainer}>
+          <AppText style={styles.header}>Settings</AppText>
+          <TouchableOpacity onPress={() => navigation.navigate("Account")}>
+            <Icon
+              name="window-close"
+              backgroundColor={colors.primary}
+              size={50}
+            />
+          </TouchableOpacity>
+        </View>
+      </Screen>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {},
+  screen: {
+    padding: Platform.OS === "android" ? 10 : 20,
+    backgroundColor: colors.light,
+  },
+  header: {
+    fontSize: 40,
+    fontWeight: "600",
+    fontStyle: "italic",
+  },
+  headerContainer: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingHorizontal: 15,
+    fontSize: 35,
+  },
 });
 export default SettingsScreen;
